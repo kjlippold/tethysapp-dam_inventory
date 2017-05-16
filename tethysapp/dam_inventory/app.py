f95c43470b8c6bb0331ccca59adef80b002dab35
Kenneth Lippold <kennethlippold@Kenneths-MBP.app.byu.edu>
2017-05-16 11:08:10 -0600

    Initial commit

---
+++ tethysapp/dam_inventory/app.py
@@ -0,0 +1,44 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class DamInventory(TethysAppBase):
+    """
+    Tethys app class for Dam Inventory.
+    """
+
+    name = 'Dam Inventory'
+    index = 'dam_inventory:home'
+    icon = 'dam_inventory/images/icon.gif'
+    package = 'dam_inventory'
+    root_url = 'dam-inventory'
+    color = '#34495e'
+    description = ''
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='dam-inventory',
+                controller='dam_inventory.controllers.home'
+            ),
+            UrlMap(
+                name='add_dam',
+                url='dam-inventory/dams/add',
+                controller='dam_inventory.controllers.add_dam'
+            ),
+            UrlMap(
+                name='dams',
+                url='dam-inventory/dams',
+                controller='dam_inventory.controllers.list_dams'
+            )
+        )
+
+        return url_maps
---
+++ tethysapp/dam_inventory/controllers.py
@@ -0,0 +1,241 @@
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .model import add_new_dam, get_all_dams
+from tethys_sdk.gizmos import MapView, Button, TextInput, DatePicker, SelectInput, MVDraw, MVView, MVLayer
+
+
+@login_required()
+def home(request):
+    """
+    Controller for the app home page.
+    """
+    # Get list of dams and create dams MVLayer:
+    dams = get_all_dams()
+    features = []
+    lat_list = []
+    lng_list = []
+
+    for dam in dams:
+        dam_location = dam.pop('location')
+        lat_list.append(dam_location['coordinates'][1])
+        lng_list.append(dam_location['coordinates'][0])
+
+        dam_feature = {
+            'type': 'Feature',
+            'geometry': {
+                'type': dam_location['type'],
+                'coordinates': dam_location['coordinates'],
+            }
+        }
+
+        features.append(dam_feature)
+
+    dams_feature_collection = {
+        'type': 'FeatureCollection',
+        'crs': {
+            'type': 'name',
+            'properties': {
+                'name': 'EPSG:4326'
+            }
+        },
+        'features': features
+    }
+
+    dams_layer = MVLayer(
+        source='GeoJSON',
+        options=dams_feature_collection,
+        legend_title='Dams'
+    )
+
+    # Define view centered on dam locations
+    try:
+        view_center = [sum(lng_list) / float(len(lng_list)), sum(lat_list) / float(len(lat_list))]
+    except ZeroDivisionError:
+        view_center = [-98.6, 39.8]
+
+    view_options = MVView(
+        projection='EPSG:4326',
+        center=view_center,
+        zoom=6,
+        maxZoom=18,
+        minZoom=2
+    )
+
+    dam_inventory_map = MapView(
+        height='100%',
+        width='100%',
+        layers=[dams_layer],
+        basemap='OpenStreetMap',
+        view=view_options
+    )
+
+    add_dam_button = Button(
+        display_text='Add Dam',
+        name='add-dam-button',
+        icon='glyphicon glyphicon-plus',
+        style='success',
+        href=reverse('dam_inventory:add_dam')
+    )
+
+    context = {
+        'dam_inventory_map': dam_inventory_map,
+        'add_dam_button': add_dam_button
+    }
+
+    return render(request, 'dam_inventory/home.html', context)
+
+
+@login_required()
+def add_dam(request):
+    """
+    Controller for the Add Dam page.
+    """
+    # Default Values
+    location = ''
+    name = ''
+    owner = 'Reclamation'
+    river = ''
+    date_built = ''
+
+    # Errors
+    location_error = ''
+    name_error = ''
+    owner_error = ''
+    river_error = ''
+    date_error = ''
+
+    # Handle form submission
+    if request.POST and 'add-button' in request.POST:
+        # Get values
+        has_errors = False
+        location = request.POST.get('geometry', None)
+        name = request.POST.get('name', None)
+        owner = request.POST.get('owner', None)
+        river = request.POST.get('river', None)
+        date_built = request.POST.get('date-built', None)
+
+        # Validate
+        if not location:
+            has_errors = True
+            location_error = 'Location is required.'
+
+        if not name:
+            has_errors = True
+            name_error = 'Name is required.'
+
+        if not owner:
+            has_errors = True
+            owner_error = 'Owner is required.'
+
+        if not river:
+            has_errors = True
+            river_error = 'River is required.'
+
+        if not date_built:
+            has_errors = True
+            date_error = 'Date Built is required.'
+
+        if not has_errors:
+            add_new_dam(location=location, name=name, owner=owner, river=river, date_built=date_built)
+            return redirect(reverse('dam_inventory:home'))
+
+        messages.error(request, "Please fix errors.")
+
+    # Define form gizmos
+    initial_view = MVView(
+        projection='EPSG:4326',
+        center=[-98.6, 39.8],
+        zoom=3.5
+    )
+
+    drawing_options = MVDraw(
+        controls=['Modify', 'Delete', 'Move', 'Point'],
+        initial='Point',
+        output_format='GeoJSON',
+        point_color='#FF0000'
+    )
+
+    location_input = MapView(
+        height='300px',
+        width='100%',
+        basemap='OpenStreetMap',
+        draw=drawing_options,
+        view=initial_view
+    )
+
+    name_input = TextInput(
+        display_text='Name',
+        name='name',
+        initial=name,
+        error=name_error
+    )
+
+    owner_input = SelectInput(
+        display_text='Owner',
+        name='owner',
+        multiple=False,
+        options=[('Reclamation', 'Reclamation'), ('Army Corp', 'Army Corp'), ('Other', 'Other')],
+        initial=owner,
+        error=owner_error
+    )
+
+    river_input = TextInput(
+        display_text='River',
+        name='river',
+        placeholder='e.g.: Mississippi River',
+        initial=river,
+        error=river_error
+    )
+
+    date_built = DatePicker(
+        name='date-built',
+        display_text='Date Built',
+        autoclose=True,
+        format='MM d, yyyy',
+        start_view='decade',
+        today_button=True,
+        initial=date_built,
+        error=date_error
+    )
+
+    add_button = Button(
+        display_text='Add',
+        name='add-button',
+        icon='glyphicon glyphicon-plus',
+        style='success',
+        attributes={'form': 'add-dam-form'},
+        submit=True
+    )
+
+    cancel_button = Button(
+        display_text='Cancel',
+        name='cancel-button',
+        href=reverse('dam_inventory:home')
+    )
+
+    context = {
+        'location_input': location_input,
+        'location_error': location_error,
+        'name_input': name_input,
+        'owner_input': owner_input,
+        'river_input': river_input,
+        'date_built_input': date_built,
+        'add_button': add_button,
+        'cancel_button': cancel_button,
+    }
+
+    return render(request, 'dam_inventory/add_dam.html', context)
+
+
+
+@login_required()
+def list_dams(request):
+    """
+    Show all dams in a table view.
+    """
+    dams = get_all_dams()
+    context = {'dams': dams}
+    return render(request, 'dam_inventory/list_dams.html', context)
+
